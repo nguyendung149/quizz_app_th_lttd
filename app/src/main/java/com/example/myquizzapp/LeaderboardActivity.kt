bdcf6cf0a91fabc6897d8a194ae4118564ce4039
ThanhTan31 <nguyenphanthanhtankbcg.ts19@gmail.com>
2023-12-10 11:08:18 +0700

    aaaaaa

---
+++ app/src/main/java/com/example/myquizzapp/LeaderboardActivity.kt
@@ -0,0 +1,4 @@
+package com.example.myquizzapp
+
+class LeaderboardActivity {
+}
--- app/src/main/java/com/example/myquizzapp/RankItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myquizzapp
-
-data class RankItem(
-    val name: String,
-    val score: Int
-)
-
